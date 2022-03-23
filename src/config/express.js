cbcbdae4cad6ec86593cdd411d690b514088af2a
João Paulo Santana Gusmão <teo.itinga@gmail.com>
2022-03-22 23:53:59 -0300

    updates

--- src/config/express.js
+++ src/config/express.js
@@ -31,9 +31,9 @@ module.exports = () => {
   }));
 
   app.use(cors({
-    //origin: '*',
+    origin: '*',
     credentials: true,
-    origin: ['http://45.174.40.217:4200'],
+    //origin: ['http://45.174.40.217:4200'],
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
   }))
 
